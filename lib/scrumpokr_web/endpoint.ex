f797c2b45d1bf427f4c03b0203e5ea1cd22e2f24
Patrick Oscity <patrick.oscity@cokron.com>
2019-04-29 13:26:42 +0200

    Make app ready for deployment on Heroku

--- lib/scrumpokr_web/endpoint.ex
+++ lib/scrumpokr_web/endpoint.ex
@@ -2,10 +2,11 @@ defmodule ScrumpokrWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :scrumpokr
 
   socket "/socket", ScrumpokrWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
-  socket "/live", Phoenix.LiveView.Socket
+  socket "/live", Phoenix.LiveView.Socket,
+    websocket: [timeout: 45_000]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
