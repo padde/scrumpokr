4eb77aacbae91939a1f2107f05bb3e7065077e8d
Patrick Oscity <patrick.oscity@cokron.com>
2019-05-09 07:33:40 +0200

    TMP set gigalixir host

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :scrumpokr, ScrumpokrWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [scheme: "https", host: "scrumpo.kr", port: 443],
+  url: [scheme: "https", host: "triangular-sentimental-electriceel.gigalixirapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
