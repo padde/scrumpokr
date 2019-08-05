b81538db51d5838b299362f0bdcce85c7238f6e4
Patrick Oscity <patrick.oscity@cokron.com>
2019-08-05 16:45:29 +0200

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -32,14 +32,14 @@ defmodule Scrumpokr.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.3"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_html, "~> 2.13.2"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view", ref: "5da3a48"}
+      {:phoenix, "~> 1.4.9"},
+      {:phoenix_pubsub, "~> 1.1.2"},
+      {:phoenix_html, "~> 2.13.3"},
+      {:phoenix_live_reload, "~> 1.2.1", only: :dev},
+      {:gettext, "~> 0.17"},
+      {:jason, "~> 1.1.2"},
+      {:plug_cowboy, "~> 2.1.0"},
+      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view", ref: "527089b"}
     ]
   end
 end
